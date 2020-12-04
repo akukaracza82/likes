00c4cb5a9f704d7f5346fff4898328ec6ad5dc87
Artur Mrozinski <amrozinski@gmail.com>
2020-12-04 14:48:54 +0000

    likes update

--- likes.rb
+++ likes.rb
@@ -1,3 +1,14 @@
+ary = []
+
 def likes(names)
-  #your code here
+  len = names.length
+  case names
+  when len == 0 then "no one likes this"
+  when len == 1 then "#{names[0]} likes this]"
+  when len == 2 then puts "#{names[0]} and #{names[1]} like this"
+  when len == 3 then puts "#{names[0]}, #{names[1]} and #{names[2]} like this"
+  when len > 3 then puts "#{names[0]}, #{names[1]} and #{names.length -2}like this"
+  end
 end
+
+p likes(ary)
